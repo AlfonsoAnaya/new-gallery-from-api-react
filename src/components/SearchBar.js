9fefa97a04b9099b970e599be5d9736cfcc0876e
AlfonsoAnaya <68916711+AlfonsoAnaya@users.noreply.github.com>
2023-06-13 20:23:49 +0200

    introduced search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,11 @@
 function SearchBar() {
     return (
         <div>
-            <p>search bar here</p>
+            <form class="form" id="form">
+                <label><input id="query-input" type="text" placeholder="plants" /></label>
+                <label>No. of photos <input id="query-number" type="number" placeholder="12" min="1" max="30" /></label>
+                <button class="btn" type="submit">Search</button>
+            </form>
         </div>
     )
 }
